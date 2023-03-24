5341b89687b48f987d169dd39d54b16e67f9c1d7
Patryk Romanowicz <rom.patryk@gmail.com>
2023-03-24 15:08:45 +0100

    fix design in tasksDone.dart

--- lib/screens/tasksDone.dart
+++ lib/screens/tasksDone.dart
@@ -11,21 +11,32 @@ class TasksDone extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Scaffold(
       backgroundColor: Colors.red[200],
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text('Tasks Done'),
+      body: Column(
+        children: [
+          Container(
+      margin: EdgeInsets.fromLTRB(50, 50, 50, 20),
+        padding: EdgeInsets.all(10),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(20)
+        ),
+        child: Text('Tasks Done', style: TextStyle(color: Colors.white),)
+        ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: tasksDone.length,
+              itemBuilder: (context, index){
+                return Center(
+                  child: Container(
+                      margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                      padding: EdgeInsets.all(10),
+                      child: Text(tasksDone[index],
+                        style: const TextStyle(color: Colors.black, fontSize: 26),)),
+                );
+              }),
+          )],
       ),
-      body: ListView.builder(
-          itemCount: tasksDone.length,
-          itemBuilder: (context, index){
-            return Center(
-              child: Container(
-                  margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                  padding: EdgeInsets.all(10),
-                  child: Text(tasksDone[index],
-                    style: const TextStyle(color: Colors.black, fontSize: 26),)),
-            );
-          }),
       bottomNavigationBar: BottomAppBar(
         color: Colors.grey[700],
         child: Row(
